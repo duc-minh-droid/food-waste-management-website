c815fb903b360912f285f84ff7c90e11e56e355e
duc-minh-droid <ducminhcsp@gmail.com>
2023-12-06 20:42:00 +0000

    Update Test.js

--- frontend/src/TestPage/Test.js
+++ frontend/src/TestPage/Test.js
@@ -50,30 +50,30 @@ export default class Example extends PureComponent {
   static demoUrl = 'https://codesandbox.io/s/simple-line-chart-kec3v';
   render() {
     return (
-    //   <ResponsiveContainer width="100%" height="100%">
-    //     <LineChart
-    //       width={500}
-    //       height={300}
-    //       data={data}
-    //       margin={{
-    //         top: 5,
-    //         right: 30,
-    //         left: 20,
-    //         bottom: 5,
-    //       }}
-    //     >
-    //       <CartesianGrid strokeDasharray="3 3" />
-    //       <XAxis dataKey="name" />
-    //       <YAxis />
-    //       <Tooltip />
-    //       <Legend />
-    //       <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-    //       <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-    //     </LineChart>
-    //   </ResponsiveContainer>
-    <LineChart width={500} height={300} data={data}>
-        <Line type="line" dataKey="react" stroke="#8884d8" />
-        </LineChart>
+        <div style={{width: '500px', height: '500px'}}>
+            <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+  
     );
   }
 }
