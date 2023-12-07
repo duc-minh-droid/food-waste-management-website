630c9f7ec8ca97d36745dcbb1c327b46a73125ef
duc-minh-droid <ducminhcsp@gmail.com>
2023-12-07 04:50:06 +0000

    added new folders

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,14 +2,16 @@ import {BrowserRouter as Router, Routes as ReactRoutes, Route} from 'react-route
 import InventoryPage from './InventoryPage/InventoryPage'
 import React from 'react'
 import RecipePage from './InventoryPage/Recipes/RecipePage/RecipePage'
+import LandingPage from './LandingPage/LandingPage'
+import ShoppingPage from './ShoppingPage/ShoppingPage'
 
 function Routes() {
     return (<Router>
         <ReactRoutes>
             <Route exact path='/inventory' element={<InventoryPage/>}/>
-            <Route path="/" element={''}>
+            <Route path="/" element={<LandingPage />}/>
+            <Route path="/shoppingList" element={<ShoppingPage />}/>
                 
-            </Route>
             <Route path='/inventory/:recipeId' element={<RecipePage />}/>
             <Route>
                 <React.Fragment>404 Not Found</React.Fragment>
